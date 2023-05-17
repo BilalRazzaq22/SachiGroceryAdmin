0bc6a228e22c53fe628b3359f48e35df8aa50288
BilalRazzaq22 <81469581+BilalRazzaq22@users.noreply.github.com>
2023-05-17 22:59:58 +0500

    change mobile number format in sms api

--- GROCERY/Controllers/OrderController.cs
+++ GROCERY/Controllers/OrderController.cs
@@ -262,6 +262,12 @@ namespace GROCERY.Controllers
         public int GenerateSMSAlert(string Masking, string toNumber, string MessageText, string MyUsername, string MyPassword)
         {
             int count = 0;
+
+            if (toNumber.Substring(0, 1).Equals("0"))
+            {
+                toNumber = toNumber.Remove(0, 1).Insert(0, "92");
+            }
+
             if (toNumber.Length == 12 && toNumber.Substring(0, 2).Equals("92"))
             {
                 count++;
--- GROCERY/Controllers/SMSController.cs
+++ GROCERY/Controllers/SMSController.cs
@@ -53,6 +53,12 @@ namespace GROCERY.Controllers
                 foreach (DataRow rowContact in cnt.Rows)
                 {
                     toNumber = rowContact["MOBILE_NO"].ToString();
+
+                    if (toNumber.Substring(0, 1).Equals("0"))
+                    {
+                        toNumber = toNumber.Remove(0, 1).Insert(0, "92");
+                    }
+
                     if (toNumber.Length == 12 && toNumber.Substring(0, 2).Equals("92"))
                     {
                         count++;
